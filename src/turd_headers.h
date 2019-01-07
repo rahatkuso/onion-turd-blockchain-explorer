3c235b1c9c4da470b521ea65fa46e2a081e9d7df
Rahat Kuso <rahatkuso@gmail.com>
2019-01-07 20:49:22 +0100

    first commit

---
+++ src/turd_headers.h
@@ -0,0 +1,48 @@
+//
+// Created by mwo on 5/11/15.
+//
+
+#ifndef TUREG01_TURD_HEADERS_H_H
+#define TUREG01_TURD_HEADERS_H_H
+
+#define DB_LMDB   2
+#define BLOCKCHAIN_DB DB_LMDB
+
+
+#define UNSIGNED_TX_PREFIX "Turd unsigned tx set\003"
+#define SIGNED_TX_PREFIX "Turd signed tx set\003"
+#define KEY_IMAGE_EXPORT_FILE_MAGIC "Turd key image export\002"
+#define OUTPUT_EXPORT_FILE_MAGIC "Turd output export\003"
+
+#define FEE_ESTIMATE_GRACE_BLOCKS 10 // estimate fee valid for that many blocks
+
+#include "version.h"
+
+#include "net/http_client.h"
+#include "storages/http_abstract_invoke.h"
+
+#include "cryptonote_core/tx_pool.h"
+#include "cryptonote_core/blockchain.h"
+#include "cryptonote_core/service_node_list.h"
+#include "cryptonote_core/service_node_deregister.h"
+#include "cryptonote_core/quorum_cop.h"
+#include "blockchain_db/lmdb/db_lmdb.h"
+#include "device/device_default.hpp"
+
+#include "wallet/wallet2.h"
+
+#include "serialization/binary_utils.h"
+
+#include "ringct/rctTypes.h"
+#include "ringct/rctOps.h"
+#include "ringct/rctSigs.h"
+
+#include "easylogging++.h"
+
+#include "common/base58.h"
+
+#include "string_coding.h"
+
+
+#endif //TUREG01_TURD_HEADERS_H_H
+
